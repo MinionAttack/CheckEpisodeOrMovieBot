2936b3a816e2f5d136e652ce62d33357e0ae5bac
MinionAttack <iago.alonso@udc.es>
2020-11-15 18:50:30 +0100

    Fixed a bug when a user did not have a username set.

--- src/command_handlers.py
+++ src/command_handlers.py
@@ -1,31 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from telegram import Update
+from telegram import Update, User
 from telegram.ext import CallbackContext
 
 from actions.find_command import process_find_options
 from actions.options_command import get_options_details
 from src.logger import logger
-from strings.command_handlers import SEE_RESULTS, STATUS_COMMAND, WELCOME_START_COMMAND, WRONG_FORMAT_ECHO_COMMAND
+from strings.command_handlers import SEE_RESULTS, STATUS_COMMAND, UNKNOWN_USER, WELCOME_START_COMMAND, WRONG_FORMAT_ECHO_COMMAND
 
 
 def start_command(update: Update, context: CallbackContext) -> None:
     """Sends a welcome message when the command /start is issued."""
-    logger.info(f"Bot started for user: {update.message.from_user.username}")
+    user_info = update.message.from_user
+    identifier = get_user(user_info)
+    logger.info(f"Bot started for user: {identifier}")
 
     update.message.reply_text(WELCOME_START_COMMAND)
 
 
 def status_command(update: Update, context: CallbackContext) -> None:
     """Sends a message to the user to check if the bot is running when the /status command is issued."""
-    logger.info(f"Status check requested by user: {update.message.from_user.username}")
+    user_info = update.message.from_user
+    identifier = get_user(user_info)
+    logger.info(f"Status check requested by user: {identifier}")
 
     update.message.reply_text(STATUS_COMMAND)
 
 
 def options_command(update: Update, context: CallbackContext) -> None:
     """Sends a message of all available commands and how to use each of them when the /options command is issued."""
-    logger.info(f"List of commands and how to use them requested by the user: {update.message.from_user.username}")
+    user_info = update.message.from_user
+    identifier = get_user(user_info)
+    logger.info(f"List of commands and how to use them requested by the user: {identifier}")
 
     options_details = get_options_details()
     update.message.reply_html(options_details)
@@ -33,19 +39,23 @@ def options_command(update: Update, context: CallbackContext) -> None:
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Sends a message when the user sends a no command."""
-    logger.info(f"A no command has been sent from user: {update.message.from_user.username}")
+    user_info = update.message.from_user
+    identifier = get_user(user_info)
+    logger.info(f"A no command has been sent from user: {identifier}")
 
     update.message.reply_text(WRONG_FORMAT_ECHO_COMMAND)
 
 
 def find_command(update: Update, context: CallbackContext) -> None:
     """Sends a message when the user request to find an episode when the /find command is issued."""
-    logger.info(f"Incoming search request from user: {update.message.from_user.username}")
+    user_info = update.message.from_user
+    identifier = get_user(user_info)
+    logger.info(f"Incoming search request from user: {identifier}")
 
     message = update.message.text.strip()
     available_options = process_find_options(message)
 
-    logger.info(f"Sending results to the user: {update.message.from_user.username}")
+    logger.info(f"Sending results to the user: {identifier}")
     photo_url = available_options.photo_url
     caption = available_options.caption
     if photo_url != '' and caption != '':
@@ -53,3 +63,17 @@ def find_command(update: Update, context: CallbackContext) -> None:
         update.message.reply_html(caption)
     elif caption != '':
         update.message.reply_text(caption)
+
+
+def get_user(user: User) -> str:
+    logger.info(f"Getting the user who sent the message.")
+
+    full_name = user.full_name
+    username = user.username
+
+    if username is not None:
+        return username
+    elif full_name != '':
+        return full_name
+    else:
+        return UNKNOWN_USER
--- strings/command_handlers.py
+++ strings/command_handlers.py
@@ -7,6 +7,8 @@ STATUS_COMMAND = f"\U0001F973 The bot is up and running! \U0001F973"
 
 TO_BE_IMPLEMENTED_OPTIONS_COMMAND = f"To be implemented!"
 
+UNKNOWN_USER = f"Unknown user, full name or username is not provided."
+
 WELCOME_START_COMMAND = f"Welcome! Use /options to see all the available options. \U0001F913"
 
 WRONG_FORMAT_ECHO_COMMAND = f"Whoops, this option is not correct. \U0001F610"
