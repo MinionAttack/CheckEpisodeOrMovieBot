4dc17d1eaf5116abcaae770aa36dbf3890fb08b3
MinionAttack <iago.alonso@udc.es>
2020-11-15 19:48:28 +0100

    Fixed a bug when the user edited a message. Telegram treats them differently if it is a new or edited message.

--- src/command_handlers.py
+++ src/command_handlers.py
@@ -11,58 +11,58 @@ from strings.command_handlers import SEE_RESULTS, STATUS_COMMAND, UNKNOWN_USER, 
 
 def start_command(update: Update, context: CallbackContext) -> None:
     """Sends a welcome message when the command /start is issued."""
-    user_info = update.message.from_user
+    user_info = update.effective_message.from_user
     identifier = get_user(user_info)
     logger.info(f"Bot started for user: {identifier}")
 
-    update.message.reply_text(WELCOME_START_COMMAND)
+    update.effective_message.reply_text(WELCOME_START_COMMAND)
 
 
 def status_command(update: Update, context: CallbackContext) -> None:
     """Sends a message to the user to check if the bot is running when the /status command is issued."""
-    user_info = update.message.from_user
+    user_info = update.effective_message.from_user
     identifier = get_user(user_info)
     logger.info(f"Status check requested by user: {identifier}")
 
-    update.message.reply_text(STATUS_COMMAND)
+    update.effective_message.reply_text(STATUS_COMMAND)
 
 
 def options_command(update: Update, context: CallbackContext) -> None:
     """Sends a message of all available commands and how to use each of them when the /options command is issued."""
-    user_info = update.message.from_user
+    user_info = update.effective_message.from_user
     identifier = get_user(user_info)
     logger.info(f"List of commands and how to use them requested by the user: {identifier}")
 
     options_details = get_options_details()
-    update.message.reply_html(options_details)
+    update.effective_message.reply_html(options_details)
 
 
 def echo(update: Update, context: CallbackContext) -> None:
     """Sends a message when the user sends a no command."""
-    user_info = update.message.from_user
+    user_info = update.effective_message.from_user
     identifier = get_user(user_info)
     logger.info(f"A no command has been sent from user: {identifier}")
 
-    update.message.reply_text(WRONG_FORMAT_ECHO_COMMAND)
+    update.effective_message.reply_text(WRONG_FORMAT_ECHO_COMMAND)
 
 
 def find_command(update: Update, context: CallbackContext) -> None:
     """Sends a message when the user request to find an episode when the /find command is issued."""
-    user_info = update.message.from_user
+    user_info = update.effective_message.from_user
     identifier = get_user(user_info)
     logger.info(f"Incoming search request from user: {identifier}")
 
-    message = update.message.text.strip()
+    message = update.effective_message.text.strip()
     available_options = process_find_options(message)
 
     logger.info(f"Sending results to the user: {identifier}")
     photo_url = available_options.photo_url
     caption = available_options.caption
     if photo_url != '' and caption != '':
-        update.message.reply_photo(photo=photo_url, caption=SEE_RESULTS)
-        update.message.reply_html(caption)
+        update.effective_message.reply_photo(photo=photo_url, caption=SEE_RESULTS)
+        update.effective_message.reply_html(caption)
     elif caption != '':
-        update.message.reply_text(caption)
+        update.effective_message.reply_text(caption)
 
 
 def get_user(user: User) -> str:
