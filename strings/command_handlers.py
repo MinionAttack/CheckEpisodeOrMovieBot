0f77f4d30336d889b3c43da5964da12a2168fe14
MinionAttack <iago.alonso@udc.es>
2020-12-01 12:50:29 +0100

    Remove emoji.

--- strings/command_handlers.py
+++ strings/command_handlers.py
@@ -4,7 +4,7 @@ SEE_RESULTS = '\U0001F389 Torrents have been found! See details below. \U0001F38
 
 STATUS_COMMAND = '\U0001F973 The bot is up and running! \U0001F973'
 
-UNKNOWN_USER = 'Unknown user, full name or username is not provided. \U0001F575'
+UNKNOWN_USER = 'Unknown user, full name or username is not provided.'
 
 WELCOME_START_COMMAND = 'Welcome! Use /help to see all the available options. \U0001F913'
 
